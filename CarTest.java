3a0c9c05ab2bf0642aaef0d08097f06f6c093411
nimashad16 <60158066+nimashad16@users.noreply.github.com>
2020-02-20 17:15:46 -0600

    Add files via upload

---
+++ CarTest.java
@@ -0,0 +1,65 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CarTest {
+	private static final int HUNDRED = 100;
+	Car car;
+	
+	@Before
+	public void setUp() throws Exception{
+		car = new Car(HUNDRED);
+	}
+	@Test
+	public void testCar() {
+		Car x = new Car(50);
+		assertFalse(car.equals(x));
+		
+		Car y = new Car(100);
+		assertTrue(car.equals(y));	}
+
+	@Test
+	public void testEqualsCar() {
+		Car x = new Car(50);
+		assertFalse(car.equals(x));
+		
+		Car y = new Car(100);
+		assertTrue(car.equals(y));
+	}
+
+	@Test
+	public void testEqualsObject() {
+		Object x = new Car(HUNDRED);
+		boolean expected = true;
+		boolean actual = car.equals(x);
+		assertEquals(expected, actual);
+		
+		String y = new String("Bogus car");
+		assertEquals(false, y.toString());
+		assertFalse(car.equals(y));
+	}
+
+	@Test
+	public void testToString() {
+		//Stick to stringent government requirements
+		//About a product information
+		String expected = "This vehicle has a range of 100 miles";
+		String actual = car.toString();
+		assertEquals(expected,actual);
+		fail("Not yet implemented");
+		
+		Car evCar = new Car(70);
+		expected = "This vehicle has a range of 700 miles";
+		actual = evCar.toString();
+		assertEquals(expected,actual);
+	}
+	@Test
+	public void testMilesToGo() {
+		int milesDriven = 10;
+		int expected = 90;
+		int actual = car.milesToGo(milesDriven);
+		assertEquals(expected, actual);
+	}
+
+}
---
+++ SuperCar.java
@@ -0,0 +1,12 @@
+
+public class SuperCar extends Car {
+	public SuperCar(int k) {
+		super(k);
+		
+	}
+	
+	public String impress() {
+		return "awesome";
+	}
+
+}
